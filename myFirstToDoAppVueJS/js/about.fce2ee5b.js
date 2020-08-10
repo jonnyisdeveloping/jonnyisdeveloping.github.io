86f3a0d348d7c6b943f9f2a24c1ee0f975e01d1d
Jonny Acevedo <jonnyisdeveloping@gmail.com>
2020-08-09 19:41:30 -0500

    removing first vue to do

--- myFirstToDoAppVueJS/js/about.fce2ee5b.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,e,n){"use strict";n.r(e);var s=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},u=[function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",{staticClass:"about"},[n("h1",[t._v("About")]),n("p",[t._v("\n    This is the TodoList app v1.0.0. It is part of the Vue crash course on Youtube.\n  ")])])}],o=n("2877"),a={},r=Object(o["a"])(a,s,u,!1,null,null,null);e["default"]=r.exports}}]);
-//# sourceMappingURL=about.fce2ee5b.js.map
